7c86c46b4284d738d64779c2c8434de5a824eac5
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-26 21:03:19 +0530

    intial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// STATICS
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Let's Build An Expense Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
